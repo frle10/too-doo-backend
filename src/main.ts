06c9a96a0761679cc907517a143e941a4e7ae61b
Ivan Skorupan <ivan.skorupan@fer.hr>
2020-10-03 15:25:08 +0200

    CORS changes

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['https://toodoo.frle.net', 'http://localhost:3000', 'http://localhost:3001'],
+    methods: 'GET, POST, PATCH',
+    credentials: true,
+    allowedHeaders: 'X-Requested-With, Accept, Content-Type, Origin'
   });
   await app.listen(3000);
 }
