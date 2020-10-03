f5e225ab9ae3d20076ee2da0e3ce3e2cb16e5945
Ivan Skorupan <ivan.skorupan@fer.hr>
2020-10-03 16:42:30 +0200

    CORS change, again...

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://toodoo.frle.net', 'http://localhost:3000', 'http://localhost:3001'],
+    origin: [
+      'https://toodoo.frle.net',
+      'http://localhost:3000',
+      'http://localhost:3001',
+    ],
     methods: 'GET, POST, PATCH',
-    credentials: true,
-    allowedHeaders: 'X-Requested-With, Accept, Content-Type, Origin'
   });
   await app.listen(3000);
 }
